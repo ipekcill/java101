60c5e20c82a04469c4e79b3d9b8bb6d544c0643c
= <=>
2023-05-04 12:46:24 +0300

    feat(BMIIndex): BMI condition has been edited

---
+++ src/Ipek/KarsilastirmaOperatorleri.java
@@ -0,0 +1,12 @@
+package Ipek;
+
+public class KarsilastirmaOperatorleri {
+    public static void main(String[] args) {
+        int A=5,B=3, C=4,D=8;
+        System.out.println(A==B);
+        System.out.println(A>=C);
+        System.out.println(C==(D/2));
+        System.out.println((A<D)&&(B>C));
+        (A<D)&&(B>C)
+    }
+}
