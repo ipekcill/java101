c5da293a274c97a74f59b165d3c66fa39775bfc2
= <=>
2023-05-04 03:39:56 +0300

    first commit

--- src/Ipek/TaksimetreHesabi.java
+++ src/Ipek/TaksimetreHesabi.java
@@ -10,12 +10,12 @@ public class TaksimetreHesabi {
     public static void main(String[] args) {
         //DATA: Kilometre başına 2.20 tl, min. ücret 2o tl (indi bindi), taksimetre açılış ücreti 10 tl,odenecektutar;
         double km,birimfiyat=2.20,odenecektutar=10;
-        int minucret=20;
+        double minucret=20.0;
         Scanner girdi=new Scanner(System.in);
         System.out.println("km bilgisi giriniz:");
         km=girdi.nextDouble();
         odenecektutar+=(km*birimfiyat);
-        odenecektutar =( odenecektutar<20)? 20: odenecektutar;
+        odenecektutar = Math.max(odenecektutar, minucret);
         System.out.println("Ödeme miktarı:"+odenecektutar);
 
     }
